0bd19041b39e992cc7f681f6d02f703a62402cce
urolyi1 <urolyi1@gmail.com>
2019-10-29 01:51:11 -0400

    Add basic parsing for authlog

---
+++ parsing_101.py
@@ -0,0 +1,37 @@
+import re
+
+def non_basedir_command(line):
+    '''
+    Function that will return the current working
+    directory of line.
+    '''
+    pattern = re.compile('cwd:([\/|\w]+) filename')
+    cwd = re.findall(pattern, line)
+    if len(cwd) == 0:
+        return []
+    else:
+        return cwd[0] 
+
+def process(line, usr_cmds):
+    ''' 
+    Function to process user commands. Currently
+    just adds command to list
+    '''
+    pattern = re.compile('filename:.+\]: (.+)')
+    cmd = pattern.search(line)
+    usr_cmds.append(cmd)
+
+def parse(fname):
+    '''
+    Parses through entire authlog file.
+    '''
+    usr_cmds = []
+    with open(fname) as file:
+        #loop through each line
+        line = file.readline()
+        while line:
+            # If cwd is in line and is not from '/' process line
+            cwd = non_basedir_command(line)
+            if cwd != [] and cwd != '/':
+                process(line, usr_cmds)
+            line = file.readline()
